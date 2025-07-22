6b833d2bbebd26fe794e936c8c4a13183730bcc6
tanbir114 <tanbir@iut-dhaka.edu>
2025-07-22 22:28:27 +0600

    Changed file name

--- server.js
+++ server.js
@@ -39,3 +39,4 @@ app.use("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // module.exports.handler = serverless(app);
+// file name changed
