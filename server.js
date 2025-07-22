6224319262cbb95a85c9e69a4d391231aa93efe6
tanbir114 <tanbir@iut-dhaka.edu>
2025-07-22 22:16:04 +0600

    Server changed

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.use("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-module.exports.handler = serverless(app);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// module.exports.handler = serverless(app);
